5a3477c0cc244000bd336a75f3ba0edbfc277d3f
MaxenMSF <alexandremione@gmail.com>
2025-12-10 14:06:44 +0100

    :lipstick: Icones remis

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="fr" className="scroll-smooth">
+      <head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+          integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </head>
       <body className={`${poppins.variable} antialiased`}>
         <Navigation />
         <main>{children}</main>
